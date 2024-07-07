02a27581082723d5148904660647e6f84d2a72c5
Mikhail Isaev <admin@ismv.ru>
2024-07-08 00:18:26 +0300

    Run uvicorn manually with asyncio

--- pyip/__main__.py
+++ pyip/__main__.py
@@ -1,4 +1,13 @@
+import asyncio
+
 import uvicorn
 
+
+async def run_dev_server():
+    config = uvicorn.Config("pyip:app", port=8000, log_level="info", reload=True)
+    server = uvicorn.Server(config)
+    await server.serve()
+
+
 if __name__ == "__main__":
-    uvicorn.run("pyip:app", host="127.0.0.1", port=8000, reload=True)
+    asyncio.run(run_dev_server())
