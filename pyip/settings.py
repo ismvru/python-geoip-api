f83af736740aeb6d278bd18ef9bd410593e07028
Mikhail Isaev <admin@ismv.ru>
2024-12-01 00:35:11 +0300

    python:S5890

--- pyip/settings.py
+++ pyip/settings.py
@@ -8,7 +8,7 @@ from pydantic_settings import BaseSettings, SettingsConfigDict
 
 class Settings(BaseSettings):
     model_config = SettingsConfigDict(env_file=".env", env_file_encoding="utf-8")
-    http_host: IPvAnyAddress = ip_address("::")
+    http_host: IPvAnyAddress = IPvAnyAddress(ip_address("::"))
     http_port: int = 8000
     geoip_city: FilePath = "/geoip/GeoLite2-City.mmdb"
     geoip_asn: FilePath = "/geoip/GeoLite2-ASN.mmdb"
